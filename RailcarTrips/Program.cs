fed76399512d92dcf22706d10bfa4b2e80716176
Sahar <sahar.fatima@arcurve.com>
2025-02-26 17:33:49 -0700

    blazor .net8 globally interactive webassembly webapp setup with http redirection issue gone

---
+++ RailcarTrips/Program.cs
@@ -0,0 +1,32 @@
+using RailcarTrips.Client.Pages;
+using RailcarTrips.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(RailcarTrips.Client._Imports).Assembly);
+
+app.Run();
